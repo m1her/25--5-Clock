fcd08eee8def2540b9cc142492930fe009fcb067
Maher Nassrallah <maher.ple2@gmail.com>
2023-05-14 22:13:48 +0300

    25/5 Clocl

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,225 @@
-import Image from 'next/image'
+"use client";
+
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
+  const audioRef = useRef(null);
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(25);
+  const [isActive, setIsActive] = useState(false);
+  const [sessionMinutes, setSessionMinutes] = useState(25);
+  const [breakMinutes, setBreakMinutes] = useState(5);
+  const [toggleTimer, setToggleTimer] = useState(false);
+
+  const secondsFormat = seconds.toString().padStart(2, "0");
+  const minutesFormat = minutes.toString().padStart(2, "0");
+
+  useEffect(() => {
+    let intervalId;
+    if (isActive) {
+      intervalId = setInterval(() => {
+        setSeconds((second) => (second <= 0 ? 59 : second - 1));
+        setMinutes((minutes) =>
+          seconds == 0 && minutes != 0 ? minutes - 1 : minutes
+        );
+      }, 1000);
+    }
+    if (minutes == 0 && seconds == 0) {
+      setToggleTimer(!toggleTimer);
+      audioRef.current.currentTime = 0;
+      audioRef.current.play();
+    }
+    return () => clearInterval(intervalId);
+  }, [isActive, seconds, minutes]);
+
+  const togglePause = () => {
+    setIsActive(!isActive);
+  };
+
+  useEffect(() => {
+    if (toggleTimer) {
+      setMinutes(breakMinutes);
+      setSeconds(0);
+    } else {
+      setMinutes(sessionMinutes);
+      setSeconds(0);
+    }
+  }, [toggleTimer]);
+
+  const resetHandler = () => {
+    setIsActive(false);
+    setMinutes(25);
+    setSessionMinutes(25);
+    setBreakMinutes(5);
+    setSeconds(0);
+  };
+
+  const incrementSessionHandler = () => {
+    if (sessionMinutes < 1 || sessionMinutes >= 60) {
+    } else {
+      if (sessionMinutes == 60) {
+        return;
+      }
+      if (toggleTimer == false) {
+        setMinutes(sessionMinutes + 1);
+        setSeconds(0);
+      }
+      setSessionMinutes(sessionMinutes + 1);
+    }
+  };
+  const decrementSessionHandler = () => {
+    if (sessionMinutes < 1 || sessionMinutes > 60) {
+    } else {
+      if (sessionMinutes == 1) {
+        return;
+      }
+      if (toggleTimer == false) {
+        setMinutes(sessionMinutes - 1);
+        setSeconds(0);
+      }
+      setSessionMinutes(sessionMinutes - 1);
+    }
+  };
+  const incrementBreakHandler = () => {
+    if (breakMinutes < 1 || breakMinutes > 60) {
+    } else {
+      if (breakMinutes == 60) {
+        return;
+      }
+      if (toggleTimer == true) {
+        setMinutes(breakMinutes + 1);
+        setSeconds(0);
+      }
+      setBreakMinutes(breakMinutes + 1);
+    }
+  };
+  const decrementBreakHandler = () => {
+    if (breakMinutes < 1 || breakMinutes >= 60) {
+    } else {
+      if (breakMinutes == 1) {
+        return;
+      }
+      if (toggleTimer == true) {
+        setMinutes(breakMinutes - 1);
+        setSeconds(0);
+      }
+      setBreakMinutes(breakMinutes - 1);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="font-mono flex min-h-screen flex-col items-center justify-between p-24 bg-[#A9907E]">
+      <div className="container mx-auto p-4">
+        <h1 className="text-4xl font-bold text-center text-[#675D50]">
+          25 + 5 Clock
+        </h1>
+        <div className="flex flex-col items-center mt-8">
+          <div className="flex space-x-8">
+            <div className="flex flex-col items-center">
+              <div
+                id="break-label"
+                className="text-xl font-semibold text-[#F3DEBA]"
+              >
+                Break Length
+              </div>
+              <div className="flex space-x-2 mt-2">
+                <button
+                  id="break-decrement"
+                  className="bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded"
+                  disabled={isActive}
+                  onClick={decrementBreakHandler}
+                >
+                  -
+                </button>
+                <div
+                  id="break-length"
+                  className="text-lg font-bold text-gray-800"
+                >
+                  {breakMinutes}
+                </div>
+                <button
+                  id="break-increment"
+                  className="bg-green-500 hover:bg-green-600 text-white px-3 py-1 rounded"
+                  disabled={isActive}
+                  onClick={incrementBreakHandler}
+                >
+                  +
+                </button>
+              </div>
+            </div>
+            <div className="flex flex-col items-center">
+              <div
+                id="session-label"
+                className="text-xl font-semibold text-[#F3DEBA]"
+              >
+                Session Length
+              </div>
+              <div className="flex space-x-2 mt-2">
+                <button
+                  id="session-decrement"
+                  className="bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded"
+                  onClick={decrementSessionHandler}
+                  disabled={isActive}
+                >
+                  -
+                </button>
+                <div
+                  id="session-length"
+                  className="text-lg font-bold text-gray-800"
+                >
+                  {sessionMinutes}
+                </div>
+                <button
+                  id="session-increment"
+                  className="bg-green-500 hover:bg-green-600 text-white px-3 py-1 rounded"
+                  onClick={incrementSessionHandler}
+                  disabled={isActive}
+                >
+                  +
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-col items-center mt-8">
+            <div
+              id="timer-label"
+              className="text-xl font-semibold text-[#F3DEBA]"
+            >
+              {toggleTimer ? "Break" : "Session"}
+            </div>
+            <div
+              id="time-left"
+              className="text-6xl font-bold text-[#675D50] mt-2"
+            >
+              {minutesFormat}:{secondsFormat}
+            </div>
+            <div className="flex space-x-4 mt-4">
+              <button
+                id="start_stop"
+                className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
+                onClick={togglePause}
+              >
+                Start/Stop
+              </button>
+              <button
+                id="reset"
+                className="bg-yellow-500 hover:bg-yellow-600 text-white px-4 py-2 rounded"
+                onClick={resetHandler}
+              >
+                Reset
+              </button>
+            </div>
+          </div>
         </div>
+
+        <audio id="beep" className="clip" ref={audioRef}>
+          <source src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav" />
+        </audio>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <p className="text-sm text-[#794a51]">
+        Coded by <span className="text-[#6f2c36]">m1her</span>
+      </p>
     </main>
-  )
+  );
 }
